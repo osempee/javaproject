c1fb0ebead7d1e3d11e9a9252056e04c53a4f2c5
lenovo <lenovo@Osempee>
2023-09-30 14:42:18 +0100

    First java project

---
+++ Peaceproject2/src/peaceproject2/Peaceproject2.java
@@ -0,0 +1,28 @@
+package peaceproject2;
+
+public class Peaceproject2 {
+
+	public static void main(String[] args) {
+		
+		String myName = "Peace";
+		int myInt = 20;
+		double shoeSize = 10.5;
+		char myInitial = 'O';
+		
+		double total = myInt + shoeSize;
+		
+		if ( myName.equals("Peace")) {
+			System.out.println("She is kind");
+		}
+		else if ( myName.equals("Monica")) {
+			System.out.println("She be scam");
+		}
+		else {
+			System.out.println("I don't know her");
+		}
+		System.out.println(total);
+		System.out.println(" My name is called "  + myName);
+
+	}
+
+}
